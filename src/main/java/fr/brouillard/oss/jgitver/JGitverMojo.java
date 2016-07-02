f621d32bb84712f38c50c63d61997d1d857277f4
Johannes Schneider <js@cedarsoft.com>
2016-07-02 11:46:40 +0200

    change order of imports

--- src/main/java/fr/brouillard/oss/jgitver/JGitverMojo.java
+++ src/main/java/fr/brouillard/oss/jgitver/JGitverMojo.java
@@ -15,13 +15,13 @@
  */
 package fr.brouillard.oss.jgitver;
 
+import java.util.List;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-
-import java.util.List;
 
 /**
  * jgitver plugin, should only be used as an extension. We use it for configuration purpose of the extension.
